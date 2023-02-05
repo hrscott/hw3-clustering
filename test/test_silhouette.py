15ccba63641c3e38d6d5d7145ffab3273668421a
Henry Scott <106702296+hrscott@users.noreply.github.com>
2023-02-04 20:11:49 -0800

    Update test_silhouette.py

--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -11,7 +11,7 @@ def test_single_cluster():
     X = np.array([[1,2],[2,3],[3,4],[4,5]])
     labels = np.zeros(X.shape[0])
     sil = cluster.Silhouette()
-    score = sil.score(X, labels)
+    score = sil.avg_score(X, labels)
     assert score == 1, f"Expected score of 1, but got {score}"
 
 # checking if function correctly handles different densities of data points within each cluster. 
@@ -19,12 +19,12 @@ def test_single_cluster():
 def test_varying_densities():
     X = np.vstack([np.random.normal(0, 1, (100, 2)), np.random.normal(5, 1, (20, 2))])
     labels = np.concatenate([np.zeros(100), np.ones(20)])
-    score = cluster.Silhouette.score(X, labels)
+    score = cluster.Silhouette.avg_score(X, labels)
     assert score > 0.5, f"Expected score > 0.5, but got {score}"
 
 #testing to see if the scoring function can correctly handle clusters with different shapes
 def test_varying_shapes():
     X = np.vstack([np.random.normal(0, 1, (100, 2)), np.random.normal(5, 3, (100, 2))])
     labels = np.concatenate([np.zeros(100), np.ones(100)])
-    score = cluster.Silhouette.score(X, labels)
+    score = cluster.Silhouette.avg_score(X, labels)
     assert abs(score) < 0.5, f"Expected score close to 0, but got {score}"
