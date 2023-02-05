de3cea3923a630c3cca1be64a48495be393dc344
Henry Scott <106702296+hrscott@users.noreply.github.com>
2023-02-04 20:41:05 -0800

    Update test_silhouette.py

--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -2,22 +2,20 @@ import pytest
 import numpy as np
 from scipy.spatial.distance import cdist
 import cluster
-import unittest
 
-class TestSilhouette(unittest.TestCase):
-    def test_score(self):
-        silhouette = cluster.Silhouette()
-        X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-        y = np.array([0, 0, 1, 1])
-        scores = silhouette.score(X, y)
-        np.testing.assert_allclose(scores, np.array([0.63, 0.63, 0.37, 0.37]))
-        
-    def test_avg_score(self):
-        silhouette = cluster.Silhouette()
-        X = np.array([[1, 2], [2, 3], [3, 4], [4, 5]])
-        y = np.array([0, 0, 1, 1])
-        avg_score = silhouette.avg_score(X, y)
-        self.assertEqual(avg_score, "Average silhouette score: 0.500")
-        
-if __name__ == '__main__':
-    unittest.main()
+
+# Test 1: Check if silhouette scores are correctly calculated
+X = np.array([[1,2],[2,3],[3,3],[3,2],[1,1]])
+y = np.array([0,0,1,1,2])
+sil = cluster.Silhouette()
+scores = sil.score(X, y)
+expected_scores = np.array([0.63, 0.63, -0.22, -0.22, 1.00])
+assert np.allclose(scores, expected_scores, atol=1e-2), f"Expected {expected_scores}, but got {scores}"
+
+# Test 2: Check if average silhouette score is correctly calculated
+X = np.array([[1,2],[2,3],[3,3],[3,2],[1,1]])
+y = np.array([0,0,1,1,2])
+sil = cluster.Silhouette()
+avg_score = sil.avg_score(X, y)
+expected_avg_score = "Average silhouette score: 0.395"
+assert avg_score == expected_avg_score, f"Expected {expected_avg_score}, but got {avg_score}"
