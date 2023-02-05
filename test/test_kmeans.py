88d82caf4b198f9d171e87a38234a8d59d933e6c
hrscott <henry.scott@ucsf.edu>
2023-02-04 19:23:50 -0800

    1

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,14 +1,32 @@
 # Write your k-means unit tests here
 import pytest
-from cluster import Kmeans
+import cluster
 import numpy as np
+from sklearn.cluster import KMeans
 
 
-#testing to see if my function provides output that's reasonably similar to that provided by sklearn
-
-# Test that the number of centroids generated is equal to the number of clusters specified in the K parameter
-def test_number_of_centroids(self):
-    kmeans = KMeans(K=5)
-    X = np.random.randn(100, 2)
+#testing to see if my function provides output that's "reasonably" similar to that provided by sklearn
+def test_kmeans_vs_sklearn():
+    np.random.seed(123)
+    X = np.random.rand(100, 2)
+    
+    kmeans = cluster.Kmeans(K=3)
     kmeans.fit(X)
-    self.assertEqual(kmeans.centroids.shape[0], 5) 
+    custom_labels = kmeans.labels
+    custom_centroids = kmeans.centroids
+    
+    sklearn_kmeans = KMeans(n_clusters=3)
+    sklearn_kmeans.fit(X)
+    sklearn_labels = sklearn_kmeans.labels_
+    sklearn_centroids = sklearn_kmeans.cluster_centers_
+    
+    assert np.array_equal(custom_labels, sklearn_labels), "Labels do not match"
+    assert np.allclose(custom_centroids, sklearn_centroids), "Centroids do not match"
+
+# testing that the number of clusters is accurate
+def test_number_of_clusters():
+    kmeans = cluster.Kmeans(K=3)
+    X = np.random.rand(100, 2)
+    kmeans.fit(X)
+    assert kmeans.K == 3
+
