ba144abc2dddbb0005442ba2d6e31daf051b7d0e
hrscott <henry.scott@ucsf.edu>
2023-02-04 18:16:22 -0800

    main_functions

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -1,74 +1,67 @@
 import numpy as np
-from scipy.spatial.distance import cdist
+from numpy.linalg import norm
 
+class Kmeans:
 
-class KMeans:
-    def __init__(self, k: int, tol: float = 1e-6, max_iter: int = 100):
-        """
-        In this method you should initialize whatever attributes will be required for the class.
+    # initializing the number of clusters, maximum number of iterations, and the random seed to be used for random first placement 
+    # of the centroids.
+    def __init__(self, K: int, max_iter=300, random_seed=123,):
+        self.K = K
+        self.max_iter = max_iter
+        self.random_seed = random_seed
 
-        You can also do some basic error handling.
+    # defining  my helper functions as private methods (i.e. my API)        
+    # selects random initial centroids from the input data.
+    def _initialize_centroids(self, X):
+        np.random.RandomState(self.random_seed)
+        random_idx = np.random.permutation(X.shape[0])
+        centroids = X[random_idx[:self.K]]
+        return centroids
+    
+    # computing the squared Euclidean distance between each data point and each centroid.
+    def _compute_euc_dist(self, X, centroids):
+        distance = np.zeros((X.shape[0], self.K))
+        for k in range(self.K):
+            row_norm = norm(X - centroids[k, :], axis=1)
+            distance[:, k] = np.square(row_norm)
+        return distance
+    
+    # assigning each data point to the closest centroid based on the computed distances.
+    def _closest_cluster(self, distance):
+        return np.argmin(distance, axis=1)
+    
+    ## Main implmentation of K-Means clustering
+    # initializes the centroids, then repeatedly updates the centroids and assigns data points to the closest centroids 
+    # until either the maximum number of iterations is reached or the centroids no longer change.
+    def fit(self, X):
+        self.centroids = self._initialize_centroids(X)
+        for i in range(self.max_iter):
+            old_centroids = self.centroids
+            distance = self._compute_euc_dist(X, old_centroids)
+            self.labels = self._closest_cluster(distance)
+            self.centroids = self.get_centroids(X, self.labels)
+            if np.all(old_centroids == self.centroids):
+                break
+        self.error = self.get_error(X, self.labels, self.centroids)
+    
 
-        What should happen if the user provides the wrong input or wrong type of input for the
-        argument k?
+    # assigning new data points to the closest cluster based on the trained centroids.
+    def predict(self, X):
+        distance = self._compute_euc_dist(X, self.centroids)
+        return self._closest_cluster(distance)
+    
 
-        inputs:
-            k: int
-                the number of centroids to use in cluster fitting
-            tol: float
-                the minimum error tolerance from previous error during optimization to quit the model fit
-            max_iter: int
-                the maximum number of iterations before quitting model fit
-        """
-
-    def fit(self, mat: np.ndarray):
-        """
-        Fits the kmeans algorithm onto a provided 2D matrix.
-        As a bit of background, this method should not return anything.
-        The intent here is to have this method find the k cluster centers from the data
-        with the tolerance, then you will use .predict() to identify the
-        clusters that best match some data that is provided.
-
-        In sklearn there is also a fit_predict() method that combines these
-        functions, but for now we will have you implement them both separately.
-
-        inputs:
-            mat: np.ndarray
-                A 2D matrix where the rows are observations and columns are features
-        """
-
-    def predict(self, mat: np.ndarray) -> np.ndarray:
-        """
-        Predicts the cluster labels for a provided matrix of data points--
-            question: what sorts of data inputs here would prevent the code from running?
-            How would you catch these sorts of end-user related errors?
-            What if, for example, the matrix is of a different number of features than
-            the data that the clusters were fit on?
-
-        inputs:
-            mat: np.ndarray
-                A 2D matrix where the rows are observations and columns are features
-
-        outputs:
-            np.ndarray
-                a 1D array with the cluster label for each of the observations in `mat`
-        """
-
-    def get_error(self) -> float:
-        """
-        Returns the final squared-mean error of the fit model. You can either do this by storing the
-        original dataset or recording it following the end of model fitting.
-
-        outputs:
-            float
-                the squared-mean error of the fit model
-        """
-
-    def get_centroids(self) -> np.ndarray:
-        """
-        Returns the centroid locations of the fit model.
-
-        outputs:
-            np.ndarray
-                a `k x m` 2D matrix representing the cluster centroids of the fit model
-        """
+    # calculating the sum of the squared distances between each data point and its assigned centroid. 
+    def get_error(self, X, labels, centroids):
+        distance = np.zeros(X.shape[0])
+        for k in range(self.K):
+            distance[labels == k] = norm(X[labels == k] - centroids[k], axis=1)
+        return np.sum(np.square(distance))
+    
+    # calculating the mean of all data points assigned to each centroid and updating the  centroids accoridngly.
+    def get_centroids(self, X, labels):
+        centroids = np.zeros((self.K, X.shape[1]))
+        for k in range(self.K):
+            centroids[k, :] = np.mean(X[labels == k, :], axis=0)
+        return centroids
+    
--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -1,6 +1,6 @@
+
 import numpy as np
 from scipy.spatial.distance import cdist
-
 
 class Silhouette:
     def __init__(self):
@@ -24,3 +24,36 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        n_samples = X.shape[0]
+        distances = cdist(X, X)
+        cluster_sizes = np.bincount(y)
+        intra_cluster_distance = np.zeros(n_samples)
+        for cluster in range(cluster_sizes.shape[0]):
+            mask = y == cluster
+            intra_cluster_distance[mask] = np.mean(distances[mask][:, mask], axis=1)
+        centroid_distance = np.zeros(n_samples)
+        for cluster in range(cluster_sizes.shape[0]):
+            mask = y != cluster
+            centroid_distance[mask] = np.mean(np.min(distances[mask][:, y == cluster], axis=1))
+        scores = (centroid_distance - intra_cluster_distance) / np.maximum(intra_cluster_distance, centroid_distance)
+        return scores
+    
+    def avg_score(self, X, y):
+        """
+        calculates the average silhouette score for all observations
+
+        inputs:
+            X: np.ndarray
+                A 2D matrix where the rows are observations and columns are features.
+
+            y: np.ndarray
+                a 1D array representing the cluster labels for each of the observations in `X`
+
+        outputs:
+            float
+                the average silhouette score for all observations
+        """
+        scores = self.score(X, y)
+        avg_score = np.mean(scores)
+        return (f"Average silhouette score: {avg_score:.3f}")
+       
